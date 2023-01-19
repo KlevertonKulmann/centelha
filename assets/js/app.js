de5f96e2afd3790a2993ce5bb1a1234e5d02bbaf
KlevertonKulmann <kleverton.kulmann@gmail.com>
2023-01-19 16:44:52 -0300

    sessão de portfolio

--- assets/js/app.js
+++ assets/js/app.js
@@ -56,31 +56,48 @@ document.addEventListener('scroll', function(){
 });
 console.log('parallax');
 function mobSlider(){
-    $('.slider-grid-four').slick({
-        slidesToScroll:1,
-        slidesToShow:4,
-        arrows:false,
-        dots:false,
-        responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 2,
-                infinite: false,
-                dots: true,
-                autoplay:true,
-                autoplaySpeed:7000
-              }
-            },
-            {
-                breakpoint: 767,
-                settings: {
-                  slidesToShow: 1,
-                  dots: true
-                }
-            },
-        ]
+  var slides = 1;
+  var $dots = false;
+  var $autoplay;
+  $('.slider-default').each(function(){
+    slides = $(this)[0].dataset.slides;
+    
+    if($(this)[0].dataset.slides !== null || $(this)[0].dataset.slides !== undefined){
+      slides = $(this)[0].dataset.slides
+    }
+    if($(this).find('div').length > slides){
+      $dots = true;
+      $autoPlay = true;
+    }else{
+      $autoPlay = false;
+    }
+    $(this).slick({
+      slidesToScroll:1,
+      slidesToShow:slides,
+      arrows:false,
+      dots:$dots,
+      autoplay:$autoPlay,
+      autoplaySpeed:7000,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            dots: true,
+            infinite: false,
+            autoplay:true,
+          }
+        },
+        {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 1,
+              dots: true,
+            }
+        },
+      ]
     });
+  })
 }
 var $cursor ;
 function toggleTema(){
