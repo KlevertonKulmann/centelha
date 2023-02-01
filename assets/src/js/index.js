910fba6a3f36a56b267ec8c19599714afcdf1f65
KlevertonKulmann <kleverton.kulmann@gmail.com>
2023-02-01 10:27:02 -0300

    Ajuste final do site

--- assets/src/js/index.js
+++ assets/src/js/index.js
@@ -38,11 +38,36 @@ function mobileMenuToggle(){
     menuBox.classList.toggle('mob-menu-aberto')
     menuBtn.classList.toggle('mob-menu-aberto')
 }
+function enviaMensagem(event){
+    event.preventDefault();
+    let form = $("#botaoEnvia").closest('form');
+    let inputName = form.find('#nome');
+    let inputEmail = form.find('#email');
+    let inputMsg = form.find('#mensagem');
+    let link = 'https://wa.me/5548991109898?text=Olá, sou '+ inputName.val() +' atendo por este telefone e pelo email: '+ inputEmail.val() +', minha solicitação seria a seguinte:'+ inputMsg.val() +'.'
+
+    window.location.href = link;
+}
+function validaForm(){
+    $('form input').on('blur', function(){
+        if(!$(this).val() || ($(this)[0].type === 'email' && !$(this).val().includes('@'))){
+            $(this).addClass("error");
+            $('#botaoEnvia').attr('disabled', 'disabled').addClass('disabled');
+        }
+        else{
+            $(this).removeClass("error");
+            if($('#nome').val() !== '' && ($('#email').val() !== '' && $(this).val().includes('@'))){
+                $('#botaoEnvia').removeAttr('disabled').removeClass('disabled');
+            }
+        }
+    });    
+}
 function init(){
     loadScreen();
     backToTop();
     anchor();
     mobSlider();
+    validaForm();
 }
 document.addEventListener( 'DOMContentLoaded', function(){
     init();
